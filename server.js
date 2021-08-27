22f7fa9dc2204e724a1eb04d5412173b4b4b4a99
Will <willstruder@gmail.com>
2021-08-27 19:11:57 -0400

    finished creating server.js file and downloading npm packages needed for project

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on PORT'));
+});
